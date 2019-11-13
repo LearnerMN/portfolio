fe829aa8c8d8a9c55defa4385d6266076aab1467
Khureltsooj <kj@shiftdsm.com>
2019-11-13 01:09:37 -0600

    added some icons and fixed bootsrap versions

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -11,7 +11,7 @@ export const AWARDS: AwardInfo[] = [
   { title: 'Best Product', color: "green", bio: 'National Creation App Contest, 2015',  description: 'developed IQ game that to solve boat and matches problem in the multi platform using Cocos2d-x game engine.' },
   { title: '2nd place', color: "green", bio: 'Unimedia Smartphone Apps Contest, 2014',  description: 'I teamed up with two people. We developed board game, called Pill Heals using Cocos2d-x game Engine for iOS and Android platform.' },
   { title: 'Special Prize', color: "green", bio: 'NAPROCK 6th International programming contest',  description: 'Participated the Naprock programming contest 2014 in Japan and won Special Prize.' },
-  { title: 'Smart Mongolia App Contest', color: "green", bio: '3rd place, Dec 2014, Mongolia',  description: 'created board game in Android platform using Cocos2d-x Game Engine.' },
+  { title: 'Smart Mongolia App Contest', color: "green", bio: '3rd place, Dec 2014, Mongolia',  description: 'Created board game in Android platform using Cocos2d-x Game Engine.' },
   { title: 'Special Prize', color: "green", bio: 'NAPROCK 5th International programming contest',  description: 'Participated the Naprock 5th international programming contest 2013 in the Japan and won Special Prize.' },
   { title: '3rd place with team', color: "green", bio: 'National Programming Contest, 2013',  description: 'National programming contest is the premiere global programming competition conducted by and for the mongolian universities.' },
   { title: 'Got student scholarship', color: "green", bio: 'Altain khuder LLC, 2014',  description: 'Altain Khuder is a Mongolian registered mining and mineral exploration company.' },
